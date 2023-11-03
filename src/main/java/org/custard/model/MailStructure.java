290365f6a1392fdfb47006874b2a3f151579447d
AmpomahWinston <51104351+Proautotype@users.noreply.github.com>
2023-11-03 17:02:49 +0000

    added recipient,

--- src/main/java/org/custard/model/MailStructure.java
+++ src/main/java/org/custard/model/MailStructure.java
@@ -1,9 +1,14 @@
 package org.custard.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MailStructure {
     private String subject;
     private String message;
+    private String recipient;
 }
